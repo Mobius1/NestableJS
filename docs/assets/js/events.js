9babb21a8c8b3e1604113bc6e80b58315b1f2589
Mobius1 <mobius1@gmx.com>
2019-10-17 08:06:58 +0100

    Update DOCS

---
+++ docs/assets/js/events.js
@@ -0,0 +1,83 @@
+const instance = new Nestable("#myList", {
+});
+
+const events = [
+	"init", "update", "start", "move", "stop", "nest", "unnest", "reorder", "error.maxdepth", "error.disabled", "error.collapsed", "error.confined"
+];
+
+const evNodes = {};
+
+instance.on("init", () => {
+	const evParent = document.getElementById("events");
+	const frag = document.createDocumentFragment();
+	for ( const event of events ) {
+		const div = document.createElement("div");
+		div.classList.add("event");
+		div.textContent = event;
+		
+		evNodes[event] = div;
+		
+		frag.appendChild(div);
+	}
+	
+	evParent.appendChild(frag);
+	
+	fireEvent("init");
+});
+
+instance.on("error.maxdepth", (el, max) => {
+	fireEvent("error.maxdepth");
+});
+
+instance.on("error.disabled", () => {
+	fireEvent("error.disabled");
+});
+
+instance.on("error.collapsed", () => {
+	fireEvent("error.collapsed");
+});
+
+instance.on("error.confined", (item, parent, newParent) => {
+	fireEvent("error.confined");
+});
+
+instance.on("update", () => {
+	fireEvent("update");
+});
+
+instance.on("start", () => {
+	fireEvent("start");
+});
+
+instance.on("move", () => {
+	fireEvent("move");
+});
+
+instance.on("reorder", () => {
+	fireEvent("reorder");
+});
+
+instance.on("nest", (newParent, oldParent) => {
+	fireEvent("nest");
+});
+
+instance.on("unnest", (newParent, oldParent) => {
+	fireEvent("unnest");
+});
+
+instance.on("stop", (data) => {
+	fireEvent("stop");
+});
+
+function fireEvent(event) {
+	evNodes[event].classList.add("active");
+	
+	if ( event.includes("error") ) {
+		evNodes[event].classList.add("error");
+	}
+	
+	setTimeout(() => {
+		evNodes[event].classList.remove("error");
+		evNodes[event].classList.remove("active");
+	}, 300);
+}
