b771a850956f050d15607a88363505f4941a6377
Mobius1 <mobius1@gmx.com>
2019-10-21 09:46:47 +0100

    Update DOCS

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -120,6 +120,7 @@ function init() {
     menuOverview.lastElementChild.innerHTML = `<li><a href="https://mobius1.github.io/NestableJS/index.html">Introduction</a></li>
                                               <li><a href="https://mobius1.github.io/NestableJS/getting-started.html">Getting Started</a></li>
                                               <li><a href="https://mobius1.github.io/NestableJS/options.html">Options</a></li>
+                                              <li><a href="https://mobius1.github.io/NestableJS/data-attributes.html">Data Attributes</a></li>
                                               <li><a href="https://mobius1.github.io/NestableJS/public-methods.html">Public Methods</a></li>
                                               <li><a href="https://mobius1.github.io/NestableJS/events.html">Events</a></li>
                                               <li><a href="https://mobius1.github.io/NestableJS/changelog.html">Changelog</a></li>`;
