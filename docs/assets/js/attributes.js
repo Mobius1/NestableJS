25ae2c42caef8a5d52d9c871f3c7b5d7a3b560ce
Mobius1 <mobius1@gmx.com>
2019-10-21 10:05:37 +0100

    Update DOCS

--- docs/assets/js/attributes.js
+++ docs/assets/js/attributes.js
@@ -1,1 +1,9 @@
 const instance = new Nestable("#myList");
+
+instance.on("error.confined", (item, parent, newParent) => {
+    parent.classList.add("nst-error");
+});
+
+instance.on("stop", (data) => {
+    instance.parent.querySelectorAll(".nst-error").forEach(el => el.classList.remove("nst-error"));
+});
